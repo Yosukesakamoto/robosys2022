830ff02b3f24fef8a2ea3d4dc86cf9b339f835db
Yosuke Sakamoto <sakamoto@DESKTOP-7KUL7E7.localdomain>
2022-10-27 12:55:25 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,3 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Yosuke sakamoto
+# SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+     echo NG at Line $1
+     res=1
+}
+
+res=0
+	    　
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng ${LINENO}
+	    　
+[ "$res" = 0 ] && echo OK 
+exit $res
